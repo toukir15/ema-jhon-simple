05560d05ba3919ff628e876b235326cf3fc7edfb
Md Tanvir Ahmed <tanvir.bd.global@gmail.com>
2023-03-27 23:21:51 +0600

    loaded add to cart btn data

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./Produnct.css";
 
 const Product = (props) => {
-  console.log(props);
   const { name, img, price, ratings, seller } = props.product;
+  const handleAddToCart = props.handleAddToCart;
   return (
     <div className="product">
       <div className="img__container">
@@ -13,9 +13,16 @@ const Product = (props) => {
         <h3 className="product__name">{name}</h3>
         <p className="product__data">Price: {price}</p>
         <p className="product__data">Manufacturer: {seller}</p>
-        <p className="product__data">Rating: {ratings} star</p>
+        <p id="product__ratings" className="product__data">
+          Rating: {ratings} star
+        </p>
       </div>
-      <button className="product__btn">Add To Cart</button>
+      <button
+        onClick={() => handleAddToCart(props.product)}
+        className="product__btn"
+      >
+        Add To Cart
+      </button>
     </div>
   );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -9,13 +9,17 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  const handleAddToCart = (product) => {
+    console.log(product);
+  };
   console.log(products);
   return (
     <div>
       <div className="shop__container">
         <div className="products__container">
           {products.map((product) => (
-            <Product product={product} />
+            <Product handleAddToCart={handleAddToCart} product={product} />
           ))}
         </div>
         <div className="cart__container">
