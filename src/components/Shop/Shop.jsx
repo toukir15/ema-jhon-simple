47fb025f6ba2ed1d76f14d7464ab8a8f63055422
Md Tanvir Ahmed <tanvir.bd.global@gmail.com>
2023-03-28 01:00:45 +0600

    show added cart item

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,6 +4,7 @@ import "./Shop.css";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
@@ -11,7 +12,8 @@ const Shop = () => {
   }, []);
 
   const handleAddToCart = (product) => {
-    console.log(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
   };
   console.log(products);
   return (
@@ -24,6 +26,7 @@ const Shop = () => {
         </div>
         <div className="cart__container">
           <h4>order summary</h4>
+          <p>Selected Items: {cart.length}</p>
         </div>
       </div>
     </div>
