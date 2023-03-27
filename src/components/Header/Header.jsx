83c9d7cdaa00cb14935b21024b2a63ed46f96085
Md Tanvir Ahmed <tanvir.bd.global@gmail.com>
2023-03-27 16:18:44 +0600

    added navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Logo from "../../images/Logo.svg";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>This is header section</h1>
-    </div>
+    <nav>
+      <img src={Logo} alt="" />
+      <div>
+        <a href="order">Order</a>
+        <a href="review">Order Review</a>
+        <a href="manage-inventory">Manage Inventory</a>
+        <a href="login">Login</a>
+      </div>
+    </nav>
   );
 };
 
