73113087e5bc84ba17f2fd860a90fa2568600921
toukir15 <toukir.developer.bd@gmail.com>
2023-03-28 17:13:08 +0600

    added cart info on ui

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import "./Cart.css";
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, shipping }) => {
+  console.log(cart);
   let totalPrice = 0;
+  let totalShipping = 0;
   for (const product of cart) {
     totalPrice = totalPrice + product.price;
+    totalShipping = totalShipping + product.shipping;
   }
+  const tax = ((totalPrice * 7.5) / 100).toFixed(2);
+  const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div>
-      <h4>order summary</h4>
+      <h2 className="cart__header">Order Summary</h2>
       <p>Selected Items: {cart.length}</p>
-      <p>Total Price:{totalPrice}</p>
-      <p>Total Shipping Charge: </p>
-      <p>Tax: </p>
-      <h6>Grand Total: </h6>
+      <p>Total Price: ${totalPrice}</p>
+      <p>Total Shipping Charge: ${totalShipping}</p>
+      <p>Tax: {tax}</p>
+      <h4>Grand Total: {grandTotal}</h4>
     </div>
   );
 };
